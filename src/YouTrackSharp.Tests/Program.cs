8567d98f586d4ba90bf2965b880b88add4d9d333
Kevin Jones <kjones@amadeusconsulting.com>
2016-01-11 17:42:45 -0700

    Update Solution With Working Build and Nuget Restore
    Also migrate Unit Tests from Nexus.Admin.Test

---
+++ src/YouTrackSharp.Tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+using NUnit.ConsoleRunner;
+
+namespace YouTrackSharp.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Runner.Main(new[] { Assembly.GetExecutingAssembly().Location });
+
+            Console.ReadKey();
+        }
+    }
+}
