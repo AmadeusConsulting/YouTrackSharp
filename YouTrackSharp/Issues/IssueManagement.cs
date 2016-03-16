f8dcda570b9e525b4fd1d52b36cc172ddd522157
Kevin Jones <kjones@amadeusconsulting.com>
2016-03-16 11:39:33 -0600

    Move Source folders to the Root of the Repository

---
+++ YouTrackSharp/Issues/IssueManagement.cs
@@ -0,0 +1,250 @@
+﻿#region License
+
+// Distributed under the BSD License
+//   
+// YouTrackSharp Copyright (c) 2010-2012, Hadi Hariri and Contributors
+// All rights reserved.
+//   
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//      * Redistributions of source code must retain the above copyright
+//         notice, this list of conditions and the following disclaimer.
+//      * Redistributions in binary form must reproduce the above copyright
+//         notice, this list of conditions and the following disclaimer in the
+//         documentation and/or other materials provided with the distribution.
+//      * Neither the name of Hadi Hariri nor the
+//         names of its contributors may be used to endorse or promote products
+//         derived from this software without specific prior written permission.
+//   
+//   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+//   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+//   PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
+//   <COPYRIGHTHOLDER> BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//   SPECIAL,EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//   LIMITED  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  ON ANY
+//   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+//   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//   
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Web;
+using YouTrackSharp.Infrastructure;
+using HttpException = EasyHttp.Infrastructure.HttpException;
+
+namespace YouTrackSharp.Issues
+{
+	public class IssueManagement
+	{
+		static readonly List<string> PresetFields = new List<string>()
+                                                     {
+                                                         "assignee", "priority", "type", "subsystem", "state",
+                                                         "fixVersions", "affectsVersions", "fixedInBuild", "summary",
+                                                         "description", "project", "permittedgroup"
+                                                     };
+		readonly IConnection _connection;
+
+		public IssueManagement(IConnection connection)
+		{
+			_connection = connection;
+		}
+
+		/// <summary>
+		/// Retrieve an issue by id
+		/// </summary>
+		/// <param name="issueId">Id of the issue to retrieve</param>
+		/// <returns>An instance of Issue if successful or InvalidRequestException if issues is not found</returns>
+		public virtual Issue GetIssue(string issueId)
+		{
+			try
+			{
+				dynamic issue = _connection.Get<Issue>(String.Format("issue/{0}", issueId));
+
+				return issue;
+			}
+			catch (HttpException exception)
+			{
+				throw new InvalidRequestException(
+						String.Format(Language.YouTrackClient_GetIssue_Issue_not_found___0_, issueId), exception);
+			}
+		}
+
+		public virtual string CreateIssue(Issue issue)
+		{
+			if (!_connection.IsAuthenticated)
+			{
+				throw new InvalidRequestException(Language.YouTrackClient_CreateIssue_Not_Logged_In);
+			}
+
+			try
+			{
+				var fieldList = issue.ToExpandoObject();
+
+				var apiResponse = _connection.Post<Issue>("rest/issue", fieldList);
+
+				var createdIssue = apiResponse.Data;
+
+				var customFields = fieldList.Where(field => !PresetFields.Contains(field.Key.ToLower())).ToDictionary(field => field.Key, field => field.Value);
+
+				foreach (var customField in customFields)
+				{
+					ApplyCommand(createdIssue.Id, string.Format("{0} {1}", customField.Key, customField.Value), string.Empty);
+				}
+				return createdIssue.Id;
+			}
+			catch (HttpException httpException)
+			{
+				throw new InvalidRequestException(httpException.StatusDescription, httpException);
+			}
+		}
+
+
+		/// <summary>
+		/// Retrieves a list of issues 
+		/// </summary>
+		/// <param name="projectIdentifier">Project Identifier</param>
+		/// <param name="max">[Optional] Maximum number of issues to return. Default is int.MaxValue</param>
+		/// <param name="start">[Optional] The number by which to start the issues. Default is 0. Used for paging.</param>
+		/// <returns>List of Issues</returns>
+		public virtual IEnumerable<Issue> GetAllIssuesForProject(string projectIdentifier, int max = int.MaxValue, int start = 0)
+		{
+			return _connection.Get<MultipleIssueWrapper, Issue>(
+				string.Format("issue/byproject/{0}", projectIdentifier),
+				new Dictionary<string, string>
+					{
+						{ "max", max.ToString(CultureInfo.InvariantCulture) },
+						{ "start", start.ToString(CultureInfo.InvariantCulture) }
+					});
+		}
+
+		/// <summary>
+		/// Retrieve comments for a particular issue
+		/// </summary>
+		/// <param name="issueId"></param>
+		/// <returns></returns>
+		public virtual IEnumerable<Comment> GetCommentsForIssue(string issueId)
+		{
+			return _connection.GetList<Comment>(String.Format("issue/comments/{0}", issueId));
+		}
+
+		public virtual bool CheckIfIssueExists(string issueId)
+		{
+			var response = _connection.Head(string.Format("issue/{0}/exists", issueId));
+			return response.StatusCode == HttpStatusCode.OK;
+		}
+
+		public virtual void AttachFileToIssue(string issuedId, string path)
+		{
+			var response = _connection.PostFile(string.Format("rest/issue/{0}/attachment", issuedId), path);
+
+			if (response.StatusCode != HttpStatusCode.Created)
+			{
+				throw new InvalidRequestException(response.StatusCode.ToString());
+			}
+		}
+
+		public virtual void ApplyCommand(string issueId, string command, string comment, bool disableNotifications = false, string runAs = "")
+		{
+			if (!_connection.IsAuthenticated)
+			{
+				throw new InvalidRequestException(Language.YouTrackClient_CreateIssue_Not_Logged_In);
+			}
+
+			try
+			{
+				dynamic commandMessage = new ExpandoObject();
+
+				commandMessage.command = command;
+				commandMessage.comment = comment;
+				if (disableNotifications)
+					commandMessage.disableNotifications = disableNotifications;
+				if (!string.IsNullOrWhiteSpace(runAs))
+					commandMessage.runAs = runAs;
+
+				_connection.Post(string.Format("rest/issue/{0}/execute", issueId), commandMessage);
+			}
+			catch (HttpException httpException)
+			{
+				throw new InvalidRequestException(httpException.StatusDescription, httpException);
+			}
+		}
+
+		public virtual void UpdateIssue(string issueId, string summary, string description)
+		{
+			if (!_connection.IsAuthenticated)
+			{
+				throw new InvalidRequestException(Language.YouTrackClient_CreateIssue_Not_Logged_In);
+			}
+
+			try
+			{
+				dynamic commandMessage = new ExpandoObject();
+
+				commandMessage.summary = summary;
+				commandMessage.description = description;
+				
+				_connection.Post(string.Format("issue/{0}", issueId), commandMessage);
+			}
+			catch (HttpException httpException)
+			{
+				throw new InvalidRequestException(httpException.StatusDescription, httpException);
+			}
+		}
+
+		public virtual IEnumerable<Issue> GetIssuesBySearch(string searchString, int max = int.MaxValue, int start = 0)
+		{
+			return _connection.Get<MultipleIssueWrapper, Issue>(
+				"issue",
+				new Dictionary<string, string>
+					{
+						{ "filter", searchString },
+						{ "max", max.ToString(CultureInfo.InvariantCulture) },
+						{ "after", start.ToString(CultureInfo.InvariantCulture) }
+					});
+		}
+
+		public virtual int GetIssueCount(string searchString)
+		{
+			var encodedQuery = HttpUtility.UrlEncode(searchString);
+
+			try
+			{
+				var count = -1;
+
+				while (count < 0)
+				{
+					var countObject = _connection.Get<Count>(string.Format("issue/count?filter={0}", encodedQuery));
+
+					count = countObject.Entity.Value;
+					Thread.Sleep(3000);
+				}
+
+				return count;
+			}
+			catch (HttpException httpException)
+			{
+				throw new InvalidRequestException(httpException.StatusDescription, httpException);
+			}
+		}
+
+		public virtual void Delete(string id)
+		{
+			_connection.Delete(string.Format("issue/{0}", id));
+		}
+
+		public virtual void DeleteComment(string issueId, string commentId, bool deletePermanently)
+		{
+			_connection.Delete(string.Format("issue/{0}/comment/{1}?permanently={2}", issueId, commentId, deletePermanently));
+		}
+	}
+}
---
+++ YouTrackSharp/Projects/ProjectManagement.cs
@@ -0,0 +1,135 @@
+﻿#region License
+
+// Distributed under the BSD License
+//   
+// YouTrackSharp Copyright (c) 2010-2012, Hadi Hariri and Contributors
+// All rights reserved.
+//   
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//      * Redistributions of source code must retain the above copyright
+//         notice, this list of conditions and the following disclaimer.
+//      * Redistributions in binary form must reproduce the above copyright
+//         notice, this list of conditions and the following disclaimer in the
+//         documentation and/or other materials provided with the distribution.
+//      * Neither the name of Hadi Hariri nor the
+//         names of its contributors may be used to endorse or promote products
+//         derived from this software without specific prior written permission.
+//   
+//   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+//   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+//   PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
+//   <COPYRIGHTHOLDER> BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//   SPECIAL,EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//   LIMITED  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  ON ANY
+//   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+//   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//   
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+
+using YouTrackSharp.Infrastructure;
+
+namespace YouTrackSharp.Projects
+{
+	public class ProjectManagement
+	{
+		#region Fields
+
+		private readonly IConnection _connection;
+
+		#endregion
+
+		#region Constructors and Destructors
+
+		public ProjectManagement(IConnection connection)
+		{
+			try
+			{
+				_connection = connection;
+			}
+			catch (ConnectionException e)
+			{
+				Console.WriteLine(e);
+			}
+		}
+
+		#endregion
+
+		#region Public Methods and Operators
+
+		public virtual void AddSubsystem(string projectName, string subsystem)
+		{
+			_connection.Put(String.Format("admin/project/{0}/subsystem/{1}", projectName, subsystem), null);
+		}
+
+		public virtual void AddVersion(Project project, ProjectVersion version)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual void DeleteVersion(string bundleName, string versionName)
+		{
+			_connection.Delete(string.Format("admin/customfield/versionBundle/{0}/{1}", bundleName, versionName));
+		}
+
+		public virtual IEnumerable<ProjectIssueTypes> GetIssueTypes()
+		{
+			return _connection.GetList<ProjectIssueTypes>("project/types");
+		}
+
+		public virtual IEnumerable<ProjectPriority> GetPriorities()
+		{
+			return _connection.GetList<ProjectPriority>("project/priorities");
+		}
+
+		public virtual Project GetProject(string projectName)
+		{
+			return _connection.Get<Project>(String.Format("admin/project/{0}", projectName));
+		}
+
+		public virtual IEnumerable<Project> GetProjects()
+		{
+			return _connection.GetList<Project>("project/all");
+		}
+
+		public virtual IEnumerable<WorkType> GetProjectWorkTypes(object projectShortname)
+		{
+			return _connection.GetList<WorkType>(string.Format("admin/project/{0}/timetracking/worktype", projectShortname));
+		}
+
+		public virtual IEnumerable<ProjectResolutionType> GetResolutions()
+		{
+			return _connection.GetList<ProjectResolutionType>("project/resolutions");
+		}
+
+		public virtual IEnumerable<ProjectState> GetStates()
+		{
+			return _connection.GetList<ProjectState>("project/states");
+		}
+
+		public virtual IEnumerable<ProjectVersion> GetVersions(string versionBundleName)
+		{
+			var x = _connection.Get<VersionBundle>(string.Format("admin/customfield/versionBundle/{0}", versionBundleName));
+			return x.Version;
+		}
+
+		public virtual IEnumerable<ProjectVersion> GetVersions(Project project)
+		{
+			return GetVersions(project.VersionBundleName());
+		}
+
+		public virtual IEnumerable<Subsystem> ListSubsystems(string projectName)
+		{
+			return _connection.GetList<Subsystem>(String.Format("admin/project/{0}/subsystem", projectName));
+		}
+
+		#endregion
+	}
+}
